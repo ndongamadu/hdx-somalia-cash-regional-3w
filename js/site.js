d1ac3b501ce7984e65b978bab0189cd42c57d649
Amadu <ndongamadou@gmail.com>
2017-04-28 00:12:09 +0000

    bubble chart

--- js/site.js
+++ js/site.js
@@ -1,16 +1,17 @@
 //configuration object
 
 var config = {
-    title:"Nepal Earthquake 2015 3W",
-    description:"Who is doing What, Where in response to the Nepal Earthquake - 06/05/2015",
-    data:"data/som-cash2.json",
+    title:"Somalia Consolidated Cash 3W",
+    description:"Who is doing What, Where in cash in Somalia - March 26, 2017",
+    data:"data/som-cash.json",
     whoFieldName:"organization",
-    whatFieldName:"what",
+    whatFieldName:"rectified",
     whereFieldName:"Adm1",
     geo:"data/som-adm1.geojson",
     joinAttribute:"REG_CODE",
     nameAttribute:"REG_NAME",
-    color:"#03a9f4"
+    color:"#03a9f4",
+    whobubble:"org_id"
 };
 
 //function to generate the 3W component
@@ -25,17 +26,33 @@ function generate3WComponent(config,data,geom){
     $('#description').html(config.description);
 
     var whoChart = dc.rowChart('#hdx-3W-who');
-    var whatChart = dc.rowChart('#hdx-3W-what');
+    var whatChart = dc.bubbleChart('#hdx-3W-what');
     var whereChart = dc.leafletChoroplethChart('#hdx-3W-where');
 
     var cf = crossfilter(data);
 
     var whoDimension = cf.dimension(function(d){ return d[config.whoFieldName]; });
-    var whatDimension = cf.dimension(function(d){ return d[config.whatFieldName]; });
+    var whatDimension = cf.dimension(function(d){ return d[config.whoFieldName]; });
     var whereDimension = cf.dimension(function(d){ return d[config.whereFieldName]; });
 
     var whoGroup = whoDimension.group();
-    var whatGroup = whatDimension.group();
+    var whatGroup = whatDimension.group().reduce(
+      function (p, v) {
+          p.totalReached += +v["reached"];
+          p.nb += +v.count;
+          return p;
+      },
+      function (p, v) {
+          p.totalReached -= +v["reached"];
+          p.nb -= +v.count;
+          if (p.nb < 0) p.nb = 0;
+          return p;
+      },
+      function () {
+          return {totalReached: 0, nb:0}
+      }
+  );
+
     var whereGroup = whereDimension.group();
     var all = cf.groupAll();
 
@@ -54,14 +71,35 @@ function generate3WComponent(config,data,geom){
     whatChart.width($('#hxd-3W-what').width()).height(400)
             .dimension(whatDimension)
             .group(whatGroup)
-            .elasticX(true)
+            .colors([config.color])
+            .colorAccessor(function(d, i){return 0;})
             .data(function(group) {
                 return group.top(15);
             })
-            .labelOffsetY(13)
-            .colors([config.color])
-            .colorAccessor(function(d, i){return 0;})
-            .xAxis().ticks(5);
+            .valueAccessor(function (d){
+              return d.value.totalReached;
+            })
+            .transitionDuration(1500)
+            .radiusValueAccessor(function (p) {
+            return p.value.nb;
+            })
+            .x(d3.scale.ordinal().domain([1,50]))
+            .xUnits(dc.units.ordinal)
+            // .y(d3.scale.linear().domain([0, 1428215]))
+            .yAxisPadding(200)
+            .r(d3.scale.linear().domain([0, 50]))
+            .elasticY(true)
+            .elasticX(true)
+            .renderHorizontalGridLines(true)
+            .renderTitle(true)
+            .title(function (p) {
+              return "Total beneficiaries reached: " +p.value.totalReached;
+            })
+            .yAxis().tickFormat(function(v){
+              return v/1000 +"K";
+            });
+
+
 
     dc.dataCount('#count-info')
             .dimension(cf)
@@ -98,11 +136,11 @@ function generate3WComponent(config,data,geom){
     var g = d3.selectAll('#hdx-3W-who').select('svg').append('g');
 
     g.append('text')
-        .attr('class', 'x-axis-label')
+        .attr('class', 'x-axis-label-todrop')
         .attr('text-anchor', 'middle')
         .attr('x', $('#hdx-3W-who').width()/2)
         .attr('y', 400)
-        .text('Activities');
+        // .text('Activities');
 
     var g = d3.selectAll('#hdx-3W-what').select('svg').append('g');
 
@@ -111,7 +149,7 @@ function generate3WComponent(config,data,geom){
         .attr('text-anchor', 'middle')
         .attr('x', $('#hdx-3W-what').width()/2)
         .attr('y', 400)
-        .text('Activities');
+        // .text('Activities');
 
     function zoomToGeom(geom){
         var bounds = d3.geo.bounds(geom);
